978954f3904575d45b0e56a7d3ad5cdb588c0c40
Jeon8719 <98005218+Jeon8719@users.noreply.github.com>
2024-02-09 21:46:50 +0900

    Update Arnavigation
    feat: GPS불러오기, 나침반&각도, POI와 거리 계산

--- 5inQ_Project_J/Assets/01.Scripts/GPSmanager.cs
+++ 5inQ_Project_J/Assets/01.Scripts/GPSmanager.cs
@@ -2,15 +2,15 @@
 using UnityEngine.UI;
 using UnityEngine.Android;
 using System.Collections;
-using System.Collections.Generic;
 
-public class GPSExample : MonoBehaviour
+public class GPSmanager : MonoBehaviour
 {
     public Text statusText;
-    
+    public float latitude;
+    public float longitude;
+
     void Start()
     {
-        
         // 위치 정보 권한 요청
         StartCoroutine(RequestLocationPermission());
     }
@@ -41,34 +41,38 @@ public class GPSExample : MonoBehaviour
         Input.location.Start();
 
         // 위치 서비스 초기화를 기다림
-        StartCoroutine(GetGPSLocation());
+        StartCoroutine(UpdateLocation());
     }
 
-    IEnumerator GetGPSLocation()
+    IEnumerator UpdateLocation()
     {
-        // 위치 정보를 가져올 때까지 대기
-        while (Input.location.status == LocationServiceStatus.Initializing)
+        while (true)
         {
+            // 위치 정보를 가져올 때까지 대기
+            while (Input.location.status == LocationServiceStatus.Initializing)
+            {
+                yield return new WaitForSeconds(1);
+            }
+
+            // 위치 정보 가져오기 성공 시
+            if (Input.location.status == LocationServiceStatus.Running)
+            {
+                // 현재 위치 정보를 구조체에 저장
+                latitude = Input.location.lastData.latitude;
+                longitude = Input.location.lastData.longitude;
+
+                // 상태 텍스트 업데이트
+                statusText.text = "현재 위치:\n" + "위도: " + latitude.ToString("F6") + "\n경도: " + longitude.ToString("F6");
+            }
+            else
+            {
+                // 위치 정보 가져오기 실패 시 메시지 표시
+                statusText.text = "위치 정보를 가져오는 데 실패했습니다.";
+            }
+
+            // 잠시 대기 후 다시 위치 업데이트
             yield return new WaitForSeconds(1);
         }
-
-        // 위치 정보 가져오기 성공 시
-        if (Input.location.status == LocationServiceStatus.Running)
-        {
-            // 위도와 경도 출력
-            float latitude = Input.location.lastData.latitude;
-            float longitude = Input.location.lastData.longitude;
-
-            statusText.text = "현재 위치:\n" + "위도: " + latitude + "\n경도: " + longitude;
-        }
-        else
-        {
-            // 위치 정보 가져오기 실패 시 메시지 표시
-            statusText.text = "위치 정보를 가져오는 데 실패했습니다.";
-        }
-
-        // 위치 서비스 종료
-        Input.location.Stop();
     }
 
     void OnDestroy()
