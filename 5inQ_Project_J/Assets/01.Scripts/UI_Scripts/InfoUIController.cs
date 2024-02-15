69b3e0ab8027e6c6d33ef9d50408a23530fa8b1e
kjw-0301 <jon9831@naver.com>
2024-02-15 16:21:46 +0900

    Feat : LoadScene_Maintain_State
    씬 넘어가는 기능 추가 및 도슨트 버튼 상태 유지 기능 추가

--- 5inQ_Project_J/Assets/01.Scripts/UI_Scripts/InfoUIController.cs
+++ 5inQ_Project_J/Assets/01.Scripts/UI_Scripts/InfoUIController.cs
@@ -1,19 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class InfoUIController : MonoBehaviour
 {
+    [SerializeField] private Text Title;
     [SerializeField] private Text Info_name;
     [SerializeField] private Text Info_Description;
     [SerializeField] private Image Info_Image;
+    [SerializeField] private Button Shooting;
+
+    [SerializeField] private string PreviousScene;
+
 
     private void Start()
     {
         AR_POI selectedARData = JsonDataHolder.Instance.GetSelectedARData();
 
-        if(selectedARData != null)
+        if (selectedARData != null)
         {
             Debug.Log("Selected Landmark: " + selectedARData.Landmark);
             Debug.Log("Selected LandmarkInfo: " + selectedARData.LandmarkInfo);
@@ -21,10 +29,31 @@ public class InfoUIController : MonoBehaviour
             Info_Description.text = selectedARData.LandmarkInfo;
 
             Sprite sprite = Resources.Load<Sprite>(selectedARData.imagepath);
-            if(sprite != null)
+            if (sprite != null)
             {
                 Info_Image.sprite = sprite;
             }
+            string selectedButton = PlayerPrefs.GetString("selectedButton", "Photozone");
+            if (selectedButton == "Photozone")
+            {
+                Title.text = "AR 포토존";
+            }
+            if (selectedButton == "Docent")
+            {
+                Title.text = "AR 도슨트";
+            }
         }
+
+
+    }
+
+    public void OnShootingButton()
+    {
+        SceneManager.LoadScene("TakeAShot");
+    }
+
+    public void OnReturnButton()
+    {
+        SceneManager.LoadScene(PreviousScene);
     }
 }
