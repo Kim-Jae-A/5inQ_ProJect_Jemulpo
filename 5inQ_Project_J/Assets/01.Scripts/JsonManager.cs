eb4882b6d1ab59b58c675d03098f9c1ab055feeb
Jeon8719 <wjstjddn8719@naver.com>
2024-02-15 13:59:42 +0900

    Update JsonManager
    fix error

--- 5inQ_Project_J/Assets/01.Scripts/JsonManager.cs
+++ 5inQ_Project_J/Assets/01.Scripts/JsonManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
 
 [System.Serializable]
@@ -20,7 +21,7 @@ public class Route
 public class TraFast
 {
     public Summary summary;
-    public float[] path;
+    public List<List<float>> path;
     public List<Section> section;
     public List<Guide> guide;
 }
@@ -78,8 +79,9 @@ public class JsonManager : MonoBehaviour
     {
         // Resources 폴더에서 JSON 파일 로드
         TextAsset json = Resources.Load<TextAsset>("Test");
+
         // JSON 파일을 RouteData 객체로 변환
-        RouteData data = JsonUtility.FromJson<RouteData>(json.text);
+        RouteData data = JsonConvert.DeserializeObject<RouteData>(json.text);
 
         // RouteData에서 필요한 정보 추출 및 출력
         Debug.Log("Code: " + data.code);
@@ -93,7 +95,12 @@ public class JsonManager : MonoBehaviour
             Debug.Log("Distance: " + firstTraFast.summary.distance);
             Debug.Log("Duration: " + firstTraFast.summary.duration);
             Debug.Log("Departure Time: " + firstTraFast.summary.departureTime);
-            Debug.Log("Path: " + firstTraFast.path.Length);
+            // path 데이터 확인
+            foreach (var point in firstTraFast.path)
+            {
+                // 리스트 내의 각 포인트 출력
+                Debug.Log("Longitude: " + point[0] + ", Latitude: " + point[1]);
+            }
 
             // Section 데이터 확인
             foreach (Section sectionInfo in firstTraFast.section)
