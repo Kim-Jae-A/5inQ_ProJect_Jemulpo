3e75f0e997ddfdbb1e9dd699c80cc3fd633c4d82
HYEJI <wl0911@naver.com>
2024-02-15 17:14:04 +0900

    Feat_ShotBtnChange
    -촬영 버튼 눌렀을 때 모드에 맞게 Sprite변경
    - 추후 변경사항 : IEnumerator 탈출 필요

--- 5inQ_Project_J/Assets/01.Scripts/UI_Scripts/CameraMode.cs
+++ 5inQ_Project_J/Assets/01.Scripts/UI_Scripts/CameraMode.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMode : MonoBehaviour
-{
-    public static bool isPicture = true;
+{   
+    public static bool isPhoto = true;
     public static bool isVideo = false;
+    public static bool isRecord = false;
+    public static bool isRecordDone = false;
 }
--- 5inQ_Project_J/Assets/01.Scripts/UI_Scripts/TakeAPhoto.cs
+++ 5inQ_Project_J/Assets/01.Scripts/UI_Scripts/TakeAPhoto.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System;
 using System.IO;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TakeAShot : MonoBehaviour
 {
-    public string path = "Assets/Resources";
+    [Header("버튼 이미지")]
+    [SerializeField] Image shotImage;
+    [SerializeField] Sprite videoStopShot;
 
     public void OnShotBtn()
     {
@@ -17,22 +20,48 @@ public class TakeAShot : MonoBehaviour
     {
         yield return new WaitForEndOfFrame();
 
-        // 캡처된 화면을 Texture2D로 생성합니다.
-        Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-        tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        tex.Apply();
+        if (CameraMode.isPhoto)
+        {
+            // 캡처된 화면을 Texture2D로 생성합니다.
+            Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+            tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+            tex.Apply();
 
-        // 캡처된 화면을 PNG 형식의 byte 배열로 변환합니다.
-        byte[] bytes = tex.EncodeToPNG();
-        Destroy(tex);
+            // 캡처된 화면을 PNG 형식의 byte 배열로 변환합니다.
+            byte[] bytes = tex.EncodeToPNG();
+            Destroy(tex);
 
-        // byte 배열을 PNG 파일로 저장합니다.
-        string fileName = "ImageName";
-        string filePath = Path.Combine(Application.persistentDataPath, fileName);
-        File.WriteAllBytes(filePath, bytes);
+            // byte 배열을 PNG 파일로 저장합니다.
+            string fileName = "ImageName";
+            string filePath = Path.Combine(Application.persistentDataPath, fileName);
+            File.WriteAllBytes(filePath, bytes);
 
-        // "SavePhoto" 씬으로 이동합니다.
-        SceneManager.LoadScene("SavePhoto");
+            // "SavePhoto" 씬으로 이동합니다.
+            SceneManager.LoadScene("SavePhoto");
+        }
+
+        if(CameraMode.isVideo)
+        {
+            shotImage.sprite = videoStopShot;
+            CameraMode.isVideo = false;
+            CameraMode.isRecord = true;
+        }
+
+        yield return null;
+
+        if(CameraMode.isRecord)
+        {         
+            CameraMode.isRecord = false;
+            CameraMode.isRecordDone = true;
+        }
+
+        yield return null;
+
+        if (CameraMode.isRecordDone)
+        {
+            SceneManager.LoadScene("SavePhoto");
+        }
+ 
     }
 
 }
