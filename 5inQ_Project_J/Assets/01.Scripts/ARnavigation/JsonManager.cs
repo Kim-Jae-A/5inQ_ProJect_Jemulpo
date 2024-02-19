aa11e61295c3a8f9fc83737b69c3ab9dfe12d282
Jeon8719 <wjstjddn8719@naver.com>
2024-02-19 16:52:07 +0900

    Update ARnavigation
    feat LineRenderer, Geospatial

--- 5inQ_Project_J/Assets/01.Scripts/ARnavigation/JsonManager.cs
+++ 5inQ_Project_J/Assets/01.Scripts/ARnavigation/JsonManager.cs
@@ -111,47 +111,15 @@ public class JsonManager : MonoBehaviour
         // JSON 파일을 RouteData 객체로 변환
         RouteData data = JsonConvert.DeserializeObject<RouteData>(json.text);
 
-        // RouteData에서 필요한 정보 추출 및 출력
+        /* RouteData에서 필요한 정보 추출 및 출력
         Debug.Log("Code: " + data.code);
         Debug.Log("Message: " + data.message);
         Debug.Log("Current Date Time: " + data.currentDateTime);
-
+        */
         // 첫 번째 TraFast 정보 가져오기
         if (data.route.trafast.Count > 0)
         {
-            TraFast firstTraFast = data.route.trafast[0];
-            /*
-            Debug.Log("Distance: " + firstTraFast.summary.distance);
-            Debug.Log("Duration: " + firstTraFast.summary.duration);
-            Debug.Log("Departure Time: " + firstTraFast.summary.departureTime);
-            // path 데이터 확인
-            
-            foreach (var point in firstTraFast.path)
-            {
-               // 리스트 내의 각 포인트 출력
-                Debug.Log("Longitude: " + point[0] + ", Latitude: " + point[1]);
-            }           
-            // Section 데이터 확인
-            foreach (Section sectionInfo in firstTraFast.section)
-            {
-                Debug.Log("Point Index: " + sectionInfo.pointIndex);
-                Debug.Log("Point Count: " + sectionInfo.pointCount);
-                Debug.Log("Distance: " + sectionInfo.distance);
-                Debug.Log("Name: " + sectionInfo.name);
-                Debug.Log("Congestion: " + sectionInfo.congestion);
-                Debug.Log("Speed: " + sectionInfo.speed);
-            }
-
-            // Guide 데이터 확인
-            foreach (Guide guideInfo in firstTraFast.guide)
-            {
-                Debug.Log("Point Index: " + guideInfo.pointIndex);
-                Debug.Log("Type: " + guideInfo.type);
-                Debug.Log("Instructions: " + guideInfo.instructions);
-                Debug.Log("Distance: " + guideInfo.distance);
-                Debug.Log("Duration: " + guideInfo.duration);
-            }
-             */
+            TraFast firstTraFast = data.route.trafast[0];      
         }
     }
 }
---
+++ 5inQ_Project_J/Assets/01.Scripts/ARnavigation/LIneDraw.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineDraw : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private Vector3[] LongLat;
+    private List<float> Longitude = new List<float>();
+    private List<float> Latitude = new List<float>();
+
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    //Json 데이터 호출 가능 여부 및 방법 확인용 코드
+    void Start()
+    {
+        LineRenderDraw();
+    }
+
+
+    void LineRenderDraw()
+    {
+        // JsonManager 클래스의 인스턴스를 통해 데이터에 접근
+        if (JsonManager.instance != null && JsonManager.instance.data != null &&
+            JsonManager.instance.data.route.trafast.Count > 0)
+        {
+            TraFast firstTraFast = JsonManager.instance.data.route.trafast[0];
+
+            // Path 데이터 확인
+            foreach (var point in firstTraFast.path)
+            {
+                Longitude.Add(point[0]);
+                Latitude.Add(point[1]);
+            }
+        }
+
+        LongLat = new Vector3[Longitude.Count];
+        for (int i = 0; i < Longitude.Count && i < Latitude.Count; i++)
+        {
+            // Longitude와 Latitude 리스트에서 Vector3로 만듭니다.
+            Vector3 point = new Vector3(Longitude[i], Latitude[i], 0);
+
+            // 변환된 Vector3를 LongLat 배열에 추가합니다.
+            LongLat[i] = point;
+        }
+
+        lineRenderer.positionCount = Longitude.Count;
+        lineRenderer.SetPositions(LongLat); // 라인 렌더러에 위치 설정
+    }
+
+}
