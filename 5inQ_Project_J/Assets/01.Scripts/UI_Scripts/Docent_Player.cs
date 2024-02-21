ee8472741b391159403aee10d8fb7515ae7c3928
kjw-0301 <147674469+kjw-0301@users.noreply.github.com>
2024-02-22 01:05:53 +0900

    Chore : Dcent_Player_Script_Update
    도슨트 플레이어 스크립트 업데이트

--- 5inQ_Project_J/Assets/01.Scripts/UI_Scripts/Docent_Player.cs
+++ 5inQ_Project_J/Assets/01.Scripts/UI_Scripts/Docent_Player.cs
@@ -11,44 +11,43 @@ public class Docent_Player : MonoBehaviour
     [SerializeField] private Button BackToDescription;
 
     float AnimationTotalLength = 0;
-    float currentTime = 0;
+    float currentLength = 0;
     // Start is called before the first frame update
     void Start()
     {
         BackToDescription.onClick.AddListener(Return_Docent_Description);
-        StartCoroutine(UpdateDocentAnimationLength());
-    }
-    private IEnumerator UpdateDocentAnimationLength()
-    {
         AnimatorClipInfo[] clipInfos = animator.GetCurrentAnimatorClipInfo(0);
         foreach (var info in clipInfos)
         {
             AnimationTotalLength += info.clip.length;
         }
-        foreach(var info in clipInfos)
-        {
-            yield return new WaitForSeconds(info.clip.length);
-
-            currentTime += info.clip.length;
-            Docent_slider.value = currentTime / AnimationTotalLength;
-        }
 
     }
-    private void Return_Docent_Description()
-    {
-        SceneManager.LoadScene("PhotoZone_Docent");
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        float clipProgress = stateInfo.normalizedTime % 1;
+        float clipLength = stateInfo.length * clipProgress;
+        if(clipLength < currentLength)
+        {
+            currentLength += stateInfo.length;
+        }
+        float totalProgress = currentLength + clipLength;
+        Docent_slider.value = totalProgress;
+        /*
         //애니메이션의 state정보를 슬라이더바에 적용
         //하지만 state가 변경될 때 마다 슬라이더바가 최신화 됨.
-        /*
         AnimatorStateInfo Docent_stateInfo = animator.GetCurrentAnimatorStateInfo(0);
         float animationLength = Docent_stateInfo.length;
         float animationTime = Docent_stateInfo.normalizedTime % 1;
         Docent_slider.value = animationTime / animationLength;
         */
     }
+ 
+    private void Return_Docent_Description()
+    {
+        SceneManager.LoadScene("PhotoZone_Docent");
+    }
+
+    // Update is called once per frame
 }
